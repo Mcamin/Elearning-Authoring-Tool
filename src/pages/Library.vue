3aacd662e06d364ff3f18c1e472a1de0adb7a515
Ahmed Amine Mchayaa <ahmedamine1993@gmail.com>
2019-12-11 00:47:46 +0100

    Added library page

---
+++ src/pages/Library.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <b-row  align-h="center" class="bg-dark vh-100"  align-content="center">
+      <b-col  class="  h-100 bg-danger " align="center" align-self="center" >
+        <div >
+          This is the login Page
+        </div>
+
+      </b-col>
+      <b-col >
+        <div>
+          This is the login Page.
+        </div>
+
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+    export default {
+        name: "Library"
+    }
+</script>
+
+<style scoped>
+
+</style>
