9d836d57b72bf33847d5f1022308b00bc5669008
Ahmed Amine Mchayaa <ahmedamine1993@gmail.com>
2019-12-17 02:24:57 +0100

    Added logging and error handling

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const  app = express();
+const morgan = require('morgan');
+
 
 const lessonRoutes = require('./api/routes/lessons');
-app.use('/products',lessonRoutes);
 const quizRoutes = require('./api/routes/quizzes');
+
+app.use(morgan('dev'));
+
+//Routes to handle requests
+app.use('/products',lessonRoutes);
 app.use('/quizzes',quizRoutes);
+
+app.use((req,res,next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error,req,res,next) => {
+  res.status(error.status || 500);
+  res.json({
+    error:{
+      message: error.message
+    }
+  })
+});
 
 
 module.exports = app;
