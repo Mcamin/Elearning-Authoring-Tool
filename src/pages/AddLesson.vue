eb65b4dbe4b75c079314bd04a1be6e8213de1df9
Patrick <p.herbke@campus.tu-berlin.de>
2020-01-02 15:06:55 +0100

    Add / Edit Title for "AddLesson"

--- src/pages/AddLesson.vue
+++ src/pages/AddLesson.vue
@@ -1,264 +1,274 @@
 <template>
-    <b-container class="mt-3">
-      <b-row align-v="center">
-        <b-col   align-self="center">
-          <!--HTML Card-->
-         <b-card no-body  class="mb-5">
-              <b-card-body>
+  <b-container class="mt-3">
+    <b-row align-v="center">
+      <b-col align-self="center">
+        <!--HTML Card-->
+        <b-card class="mb-5" no-body>
+          <b-card-body>
 
-                <b-card-text>
-                  <b-container fluid>
-                    <b-row>
-                      <b-col class="mb-3">
-                        <span class="h4">Lesson Title</span>
-                        <a href="#" class="ml-2">
-                          <font-awesome-icon :icon="['fas', 'pen']" size="xs"  />
-                        </a>
-                        <a href="#" class="ml-2">
-                          <font-awesome-icon :icon="['fas', 'trash']" size="xs"  />
-                        </a>
-                        <a href="#" class="ml-2">
-                          <font-awesome-icon :icon="['fas', 'cog']" size="xs"  />
-                        </a>
-                      </b-col>
+            <b-card-text>
+              <b-container fluid>
+                <b-row>
+                  <b-col class="mb-3">
 
-                    </b-row>
-                    <b-row>
-                      <b-col class="mb-5">
-                        <el-tag
-                          :key="tag"
-                          v-for="tag in dynamicTags"
-                          closable
-                          :disable-transitions="false"
-                          @close="handleClose(tag)">
-                          {{tag}}
-                        </el-tag>
-                        <el-input
-                          class="input-new-tag"
-                          v-if="inputVisible"
-                          v-model="inputValue"
-                          ref="saveTagInput"
-                          size="mini"
-                          @keyup.enter.native="handleInputConfirm"
-                          @blur="handleInputConfirm"
-                        >
-                        </el-input>
-                        <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+                    <input :class="{view: !isEditing}" :disabled="!isEditing" :value="lesson.title" ref="lesson_title"
+                           type="text">
+                    <a class="ml-2" href="#">
+                      <font-awesome-icon :icon="['fas', 'pen']" @click="isEditing = !isEditing" size="xs"
+                                         v-if="!isEditing"/>
 
+                      <font-awesome-icon :icon="['fas', 'save']" @click="save" size="s" v-else-if="isEditing"/>
 
-                      </b-col>
+                    </a>
+                    <a class="ml-2" href="#">
+                      <font-awesome-icon :icon="['fas', 'trash']" size="xs"/>
+                    </a>
+                    <a class="ml-2" href="#">
+                      <font-awesome-icon :icon="['fas', 'cog']" size="xs"/>
+                    </a>
+                  </b-col>
 
-                    </b-row>
-                  </b-container>
-
-
-                  <vue-editor v-model="content"></vue-editor>
-
-                </b-card-text>
-              </b-card-body>
+                </b-row>
+                <b-row>
+                  <b-col class="mb-5">
+                    <el-tag
+                      :disable-transitions="false"
+                      :key="tag"
+                      @close="handleClose(tag)"
+                      closable
+                      v-for="tag in dynamicTags">
+                      {{tag}}
+                    </el-tag>
+                    <el-input
+                      @blur="handleInputConfirm"
+                      @keyup.enter.native="handleInputConfirm"
+                      class="input-new-tag"
+                      ref="saveTagInput"
+                      size="mini"
+                      v-if="inputVisible"
+                      v-model="inputValue"
+                    >
+                    </el-input>
+                    <el-button @click="showInput" class="button-new-tag" size="small" v-else>+ New Tag</el-button>
 
 
+                  </b-col>
+
+                </b-row>
+              </b-container>
 
 
+              <vue-editor v-model="content"></vue-editor>
 
-            </b-card>
-
-          <!--Metadata Card-->
-          <b-card no-body class="mb-5">
-            <b-card-body>
-
-              <b-card-text>
-                <b-container fluid>
-                  <b-row>
-                    <b-col class="mb-3">
-                      <h6>Metadata</h6>
-                      <span class="font-italic">Before publishing the course, meta data need to be inserted.</span>
-                    </b-col>
-
-                  </b-row>
-                  <b-row>
-                    <b-col class="mb-2">
-                      <b-card no-body>
-                        <b-tabs card>
-                          <b-tab no-body title="LIFECYCLE">
-                            <b-container>
-                              <b-row >
-
-                                <b-col>
-                                  <b-card no-body  class="my-3">
-                                    <b-card-body>
-
-                                      <b-card-text>
-                                        <b-container>
-                                          <b-row>
-                                            <b-col class="mb-3">
-                                              <h5 >Structure</h5>
-                                              <span>Description</span>
-                                            </b-col>
-                                            <b-col class="mb-3">
-                                              <span>SOURCE</span><br>
-                                              <b-dropdown id="dropdown-1" text="SLHw" class="mt-2">
-                                                <b-dropdown-item>First Action</b-dropdown-item>
-                                                <b-dropdown-item>Second Action</b-dropdown-item>
-                                                <b-dropdown-item>Third Action</b-dropdown-item>
-                                              </b-dropdown>
-                                            </b-col>
-                                            <b-col class="mb-3">
-                                              <span>STATE</span><br>
-                                              <b-dropdown id="dropdown-1" text="SLHw" class="mt-2">
-                                                <b-dropdown-item>First Action</b-dropdown-item>
-                                                <b-dropdown-item>Second Action</b-dropdown-item>
-                                                <b-dropdown-item>Third Action</b-dropdown-item>
-                                              </b-dropdown>
-                                            </b-col>
-
-                                          </b-row>
-
-                                        </b-container>
+            </b-card-text>
+          </b-card-body>
 
 
+        </b-card>
 
-                                      </b-card-text>
-                                    </b-card-body>
-                                  </b-card>
-                                </b-col>
-                              </b-row>
-                              <b-row >
+        <!--Metadata Card-->
+        <b-card class="mb-5" no-body>
+          <b-card-body>
 
-                                <b-col>
-                                  <b-card no-body  class="my-3">
-                                    <b-card-body>
+            <b-card-text>
+              <b-container fluid>
+                <b-row>
+                  <b-col class="mb-3">
+                    <h6>Metadata</h6>
+                    <span class="font-italic">Before publishing the course, meta data need to be inserted.</span>
+                  </b-col>
 
-                                      <b-card-text>
-                                        <b-container>
-                                          <b-row>
-                                            <b-col class="mb-3">
-                                              <h5 >Structure</h5>
-                                              <span>Description</span>
-                                            </b-col>
-                                            <b-col class="mb-3">
-                                              <span>SOURCE</span><br>
-                                              <b-dropdown id="dropdown-1" text="SLHw" class="mt-2">
-                                                <b-dropdown-item>First Action</b-dropdown-item>
-                                                <b-dropdown-item>Second Action</b-dropdown-item>
-                                                <b-dropdown-item>Third Action</b-dropdown-item>
-                                              </b-dropdown>
-                                            </b-col>
-                                            <b-col class="mb-3">
-                                              <span>STATE</span><br>
-                                              <b-dropdown id="dropdown-1" text="SLHw" class="mt-2">
-                                                <b-dropdown-item>First Action</b-dropdown-item>
-                                                <b-dropdown-item>Second Action</b-dropdown-item>
-                                                <b-dropdown-item>Third Action</b-dropdown-item>
-                                              </b-dropdown>
-                                            </b-col>
+                </b-row>
+                <b-row>
+                  <b-col class="mb-2">
+                    <b-card no-body>
+                      <b-tabs card>
+                        <b-tab no-body title="LIFECYCLE">
+                          <b-container>
+                            <b-row>
 
-                                          </b-row>
+                              <b-col>
+                                <b-card class="my-3" no-body>
+                                  <b-card-body>
 
-                                        </b-container>
+                                    <b-card-text>
+                                      <b-container>
+                                        <b-row>
+                                          <b-col class="mb-3">
+                                            <h5>Structure</h5>
+                                            <span>Description</span>
+                                          </b-col>
+                                          <b-col class="mb-3">
+                                            <span>SOURCE</span><br>
+                                            <b-dropdown class="mt-2" id="dropdown-1" text="SLHw">
+                                              <b-dropdown-item>First Action</b-dropdown-item>
+                                              <b-dropdown-item>Second Action</b-dropdown-item>
+                                              <b-dropdown-item>Third Action</b-dropdown-item>
+                                            </b-dropdown>
+                                          </b-col>
+                                          <b-col class="mb-3">
+                                            <span>STATE</span><br>
+                                            <b-dropdown class="mt-2" id="dropdown-1" text="SLHw">
+                                              <b-dropdown-item>First Action</b-dropdown-item>
+                                              <b-dropdown-item>Second Action</b-dropdown-item>
+                                              <b-dropdown-item>Third Action</b-dropdown-item>
+                                            </b-dropdown>
+                                          </b-col>
 
+                                        </b-row>
 
-
-                                      </b-card-text>
-                                    </b-card-body>
-                                  </b-card>
-                                </b-col>
-                              </b-row>
-                            </b-container>
-
-
-                          </b-tab>
-
-                          <b-tab no-body title="TECHNICAL">
-
-                          </b-tab>
-
-                          <b-tab no-body title="EDUCATIONAL">
-
-                          </b-tab>
-
-                          <b-tab title="ANNOTATION">
-
-                          </b-tab>
-                        </b-tabs>
-                      </b-card>
-
-                    </b-col>
-
-                  </b-row>
-                </b-container>
+                                      </b-container>
 
 
+                                    </b-card-text>
+                                  </b-card-body>
+                                </b-card>
+                              </b-col>
+                            </b-row>
+                            <b-row>
 
-              </b-card-text>
-            </b-card-body>
+                              <b-col>
+                                <b-card class="my-3" no-body>
+                                  <b-card-body>
+
+                                    <b-card-text>
+                                      <b-container>
+                                        <b-row>
+                                          <b-col class="mb-3">
+                                            <h5>Structure</h5>
+                                            <span>Description</span>
+                                          </b-col>
+                                          <b-col class="mb-3">
+                                            <span>SOURCE</span><br>
+                                            <b-dropdown class="mt-2" id="dropdown-1" text="SLHw">
+                                              <b-dropdown-item>First Action</b-dropdown-item>
+                                              <b-dropdown-item>Second Action</b-dropdown-item>
+                                              <b-dropdown-item>Third Action</b-dropdown-item>
+                                            </b-dropdown>
+                                          </b-col>
+                                          <b-col class="mb-3">
+                                            <span>STATE</span><br>
+                                            <b-dropdown class="mt-2" id="dropdown-1" text="SLHw">
+                                              <b-dropdown-item>First Action</b-dropdown-item>
+                                              <b-dropdown-item>Second Action</b-dropdown-item>
+                                              <b-dropdown-item>Third Action</b-dropdown-item>
+                                            </b-dropdown>
+                                          </b-col>
+
+                                        </b-row>
+
+                                      </b-container>
 
 
+                                    </b-card-text>
+                                  </b-card-body>
+                                </b-card>
+                              </b-col>
+                            </b-row>
+                          </b-container>
 
 
+                        </b-tab>
 
-          </b-card>
+                        <b-tab no-body title="TECHNICAL">
 
-        </b-col>
-      </b-row>
-    </b-container>
+                        </b-tab>
+
+                        <b-tab no-body title="EDUCATIONAL">
+
+                        </b-tab>
+
+                        <b-tab title="ANNOTATION">
+
+                        </b-tab>
+                      </b-tabs>
+                    </b-card>
+
+                  </b-col>
+
+                </b-row>
+              </b-container>
+
+
+            </b-card-text>
+          </b-card-body>
+
+
+        </b-card>
+
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-    import { library } from '@fortawesome/fontawesome-svg-core'
-    import { faPen, faTrash,faCog,faSortUp,faSortDown ,faPlusCircle} from '@fortawesome/free-solid-svg-icons'
-    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-    import { VueEditor } from "vue2-editor";
-    import {Tag,Button,Input} from 'element-ui'
+  import {library} from '@fortawesome/fontawesome-svg-core'
+  import {faCog, faPen, faSave, faTrash} from '@fortawesome/free-solid-svg-icons'
+  import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+  import {VueEditor} from "vue2-editor";
+  import {Button, Input, Tag} from 'element-ui'
 
-    library.add(
-        faPen,
-        faTrash,
-        faCog,
-    );
+  library.add(
+    faPen,
+    faTrash,
+    faCog,
+    faSave
+  );
 
   export default {
-        name: "AddLesson",
-      data() {
-          return {
-              dynamicTags: ['Tag 1', 'Tag 2', 'Tag 3'],
-              inputVisible: false,
-              inputValue: ''
-          };
+    name: "AddLesson",
+    data() {
+      return {
+        dynamicTags: ['Tag 1', 'Tag 2', 'Tag 3'],
+        inputVisible: false,
+        inputValue: '',
+        isEditing: false,
+        lesson: {
+          title: '...'
+        }
+      };
+    },
+    methods: {
+      handleClose(tag) {
+        this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
       },
-      methods: {
-          handleClose(tag) {
-              this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
-          },
 
-          showInput() {
-              this.inputVisible = true;
-              this.$nextTick(_ => {
-                  this.$refs.saveTagInput.$refs.input.focus();
-              });
-          },
+      showInput() {
+        this.inputVisible = true;
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus();
+        });
+      },
 
-          handleInputConfirm() {
-              let inputValue = this.inputValue;
-              if (inputValue) {
-                  this.dynamicTags.push(inputValue);
-              }
-              this.inputVisible = false;
-              this.inputValue = '';
-          }
-
-  },
-      components:{
-          'font-awesome-icon': FontAwesomeIcon,
-          VueEditor,
-          Tag,
-          Input,
-          Button
-
+      handleInputConfirm() {
+        let inputValue = this.inputValue;
+        if (inputValue) {
+          this.dynamicTags.push(inputValue);
+        }
+        this.inputVisible = false;
+        this.inputValue = '';
+      },
+      save() {
+        this.lesson.title = this.$refs['lesson_title'].value;
+        console.log("TEST");
+        this.isEditing = !this.isEditing;
       }
+    },
+    components: {
+      'font-awesome-icon': FontAwesomeIcon,
+      VueEditor,
+      Tag,
+      Input,
+      Button
+
     }
+  }
 </script>
 
 <style scoped>
-
+  .view {
+    border-color: transparent;
+    background-color: initial;
+    color: initial
+  }
 </style>
