a81c2b4f9148da1f705a94dd5c648331734cdc73
Ahmed Amine <ahmedamine1993@gmail.com>
2020-02-01 14:34:42 +0100

    Finished Creating steps

--- src/components/Modals/AddSectionModuleModal.vue
+++ src/components/Modals/AddSectionModuleModal.vue
@@ -1,14 +1,14 @@
 <template>
-    <b-modal id="add-section-module" centered :title="headerTitle" hide-footer size="lg">
+    <b-modal id="add-section-module" centered :title="strings.headerTitle" hide-footer size="lg">
       <b-container>
-        <el-steps :active="active" finish-status="success" align-center>
-          <el-step title="Add a Section or a Module">
-          </el-step>
-          <el-step title="Step 2"></el-step>
-          <el-step title="Step 3"></el-step>
+        <el-steps :active="params.stepper" finish-status="success" align-center>
+          <el-step/>
+          <el-step/>
+          <el-step/>
+          <el-step/>
         </el-steps>
 
-        <b-row v-if="active===1" class=" text-center mb-3 mt-3">
+        <b-row v-if="params.stepper===0" class=" text-center mb-3 mt-5">
 
           <!-- Add Section -->
           <b-col>
@@ -20,9 +20,9 @@
                 </template>
 
                 <b-card-body>
-                  <b-card-title>{{addSection.title}}</b-card-title>
+                  <b-card-title>{{strings.addSection.title}}</b-card-title>
                   <b-card-text>
-                    {{addSection.description}}
+                    {{strings.addSection.description}}
                   </b-card-text>
                 </b-card-body>
               </b-card>
@@ -40,9 +40,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{addModule.title}}</b-card-title>
+                <b-card-title>{{strings.addModule.title}}</b-card-title>
                 <b-card-text>
-                  {{addModule.description}}
+                  {{strings.addModule.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -59,9 +59,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{importSectionModule.title}}</b-card-title>
+                <b-card-title>{{strings.importSectionModule.title}}</b-card-title>
                 <b-card-text>
-                  {{importSectionModule.description}}
+                  {{strings.importSectionModule.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -71,7 +71,7 @@
         </b-row>
 
         <!-- Add Section clicked -->
-        <b-row v-if="active===1" class=" text-center d-flex justify-content-center mb-3 mt-3">
+        <b-row v-if="params.stepper===1 && params.showAddModule" class=" text-center d-flex justify-content-center mb-3 mt-5">
           <!-- Add Module -->
           <b-col cols="4">
             <a @click.prevent="addModuleClicked()">
@@ -82,9 +82,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{addModule.title}}</b-card-title>
+                <b-card-title>{{strings.addModule.title}}</b-card-title>
                 <b-card-text>
-                  {{addModule.description}}
+                  {{strings.addModule.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -101,9 +101,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{importModule.title}}</b-card-title>
+                <b-card-title>{{strings.importModule.title}}</b-card-title>
                 <b-card-text>
-                  {{importModule.description}}
+                  {{strings.importModule.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -114,7 +114,7 @@
         <!-- End Add Section clicked -->
 
         <!-- Add Module clicked -->
-        <b-row v-if="active===0" class=" text-center mb-3 mt-3">
+        <b-row v-if="params.showAddContent && (params.stepper===1 || params.stepper===2)" class=" text-center mb-3 mt-5">
           <!-- Add Interaction -->
           <b-col>
             <a @click.prevent="addInteractionClicked()">
@@ -125,9 +125,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{addInteraction.title}}</b-card-title>
+                <b-card-title>{{strings.addInteraction.title}}</b-card-title>
                 <b-card-text>
-                  {{addInteraction.description}}
+                  {{strings.addInteraction.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -145,9 +145,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{addLesson.title}}</b-card-title>
+                <b-card-title>{{strings.addLesson.title}}</b-card-title>
                 <b-card-text>
-                  {{addLesson.description}}
+                  {{strings.addLesson.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -164,9 +164,9 @@
               </template>
 
               <b-card-body>
-                <b-card-title>{{addGlossary.title}}</b-card-title>
+                <b-card-title>{{strings.addGlossary.title}}</b-card-title>
                 <b-card-text>
-                  {{addGlossary.description}}
+                  {{strings.addGlossary.description}}
                 </b-card-text>
               </b-card-body>
             </b-card>
@@ -175,6 +175,135 @@
           <!-- End Add Glossary -->
         </b-row>
         <!-- End Add Module clicked -->
+
+        <!-- Add Interaction clicked -->
+        <b-row v-if="params.showInteractions && (params.stepper===2 || params.stepper===3)" class=" text-center d-flex justify-content-center mb-3 mt-5">
+          <!-- Create Interaction -->
+          <b-col cols="4">
+            <a @click.prevent="createInteractionClicked()">
+              <b-card no-body style="max-width: 14rem;">
+
+                <template v-slot:header>
+                  <font-awesome-icon :icon="['fas', 'list-ul']" size="lg" />
+                </template>
+
+                <b-card-body>
+                  <b-card-title>{{strings.createInteraction.title}}</b-card-title>
+                  <b-card-text>
+                    {{strings.createInteraction.description}}
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </a>
+          </b-col>
+          <!-- End Create Interaction -->
+
+          <!-- Import Interaction -->
+          <b-col  cols="4">
+            <a @click.prevent="importInteractionClicked()">
+              <b-card no-body style="max-width: 14rem;">
+                <template v-slot:header>
+                  <font-awesome-icon :icon="['fas', 'file-upload']" size="lg" />
+                </template>
+
+                <b-card-body>
+                  <b-card-title>{{strings.importInteraction.title}}</b-card-title>
+                  <b-card-text>
+                    {{strings.importInteraction.description}}
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </a>
+          </b-col>
+          <!-- End Import Interaction -->
+        </b-row>
+        <!-- End Add Interaction clicked -->
+
+        <!-- Add Lesson clicked -->
+        <b-row v-if="params.showLesson && (params.stepper===2 || params.stepper===3)" class=" text-center d-flex justify-content-center mb-3 mt-5">
+          <!-- Create a lesson -->
+          <b-col cols="4">
+            <a @click.prevent="createLessonClicked()">
+              <b-card no-body style="max-width: 14rem;">
+
+                <template v-slot:header>
+                  <font-awesome-icon :icon="['fas', 'list-ul']" size="lg" />
+                </template>
+
+                <b-card-body>
+                  <b-card-title>{{strings.createLesson.title}}</b-card-title>
+                  <b-card-text>
+                    {{strings.createLesson.description}}
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </a>
+          </b-col>
+          <!-- End Create a  lesson -->
+
+          <!-- Import lesson -->
+          <b-col  cols="4">
+            <a @click.prevent="importLessonClicked()">
+              <b-card no-body style="max-width: 14rem;">
+                <template v-slot:header>
+                  <font-awesome-icon :icon="['fas', 'file-upload']" size="lg" />
+                </template>
+
+                <b-card-body>
+                  <b-card-title>{{strings.importLesson.title}}</b-card-title>
+                  <b-card-text>
+                    {{strings.importLesson.description}}
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </a>
+          </b-col>
+          <!-- End Import lesson -->
+        </b-row>
+        <!-- End Add Lesson clicked -->
+
+        <!-- Add Glossary clicked -->
+        <b-row v-if="params.showGlossary && (params.stepper===2 || params.stepper===3)" class=" text-center d-flex justify-content-center mb-3 mt-5">
+          <!-- Add Module -->
+          <b-col cols="4">
+            <a @click.prevent="createGlossaryClicked()">
+              <b-card no-body style="max-width: 14rem;">
+
+                <template v-slot:header>
+                  <font-awesome-icon :icon="['fas', 'list-ul']" size="lg" />
+                </template>
+
+                <b-card-body>
+                  <b-card-title>{{strings.createGlossary.title}}</b-card-title>
+                  <b-card-text>
+                    {{strings.createGlossary.description}}
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </a>
+          </b-col>
+          <!-- End Add Module -->
+
+          <!-- Import Module -->
+          <b-col  cols="4">
+            <a @click.prevent="importGlossaryClicked()">
+              <b-card no-body style="max-width: 14rem;">
+                <template v-slot:header>
+                  <font-awesome-icon :icon="['fas', 'file-upload']" size="lg" />
+                </template>
+
+                <b-card-body>
+                  <b-card-title>{{strings.importGlossary.title}}</b-card-title>
+                  <b-card-text>
+                    {{strings.importGlossary.description}}
+                  </b-card-text>
+                </b-card-body>
+              </b-card>
+            </a>
+          </b-col>
+          <!-- End Import Module -->
+        </b-row>
+        <!-- End Add Glossary clicked -->
 
       </b-container>
     </b-modal>
@@ -200,52 +329,114 @@
       'font-awesome-icon': FontAwesomeIcon},
       data() {
         return {
-          active: 0,
-          headerTitle:'Add a new Section/Module',
-          addSection:{
-            title:'Add Section',
-            description: 'Add a section to the course to organize its structure'
+          params:{
+            stepper: 0,
+            showAddModule: false,
+            showAddContent: false,
+            showInteractions: false,
+            showLesson: false,
+            showGlossary: false
           },
-          addModule:{
-            title:'Add Module',
-            description: 'Add a Module to the course.'
-          },
-          importSectionModule:{
-            title:'Import Section/Module',
-            description: 'Import a section/module from the library'
-          },
-          importModule:{
-            title:'Import Module',
-            description: 'Import a module from the library'
-          },
-          addInteraction:{
-            title:'Add Interaction',
-            description: 'Add a Quiz to the module'
-          },
-          addLesson:{
-            title:'Add Lesson',
-            description: 'Add a Lesson to the module'
-          },
-          addGlossary:{
-            title:'Add Glossary',
-            description: 'Add a Glossary to the module'
-          },
-
-          id : null,
-          formData: {
-            title: '',
+          strings: {
+            headerTitle: 'Add a new Section/Module',
+            addSection: {
+              title: 'Add Section',
+              description: 'Add a section to the course to organize its structure'
             },
-          show:true
+            addModule: {
+              title: 'Add Module',
+              description: 'Add a Module to the course.'
+            },
+            importSectionModule: {
+              title: 'Import Section/Module',
+              description: 'Import a section/module from the library'
+            },
+            importModule: {
+              title: 'Import Module',
+              description: 'Import a module from the library'
+            },
+            addInteraction: {
+              title: 'Add Interaction',
+              description: 'Add a Quiz to the module'
+            },
+            addLesson: {
+              title: 'Add Lesson',
+              description: 'Add a Lesson to the module'
+            },
+            addGlossary: {
+              title: 'Add Glossary',
+              description: 'Add a Glossary to the module'
+            },
+            createLesson:{
+              title: 'Create Lesson',
+              description: 'Create a new lesson'
+            },
+            createInteraction:{
+              title: 'Create Interaction',
+              description: 'Create a new interaction'
+            },
+            createGlossary:{
+              title: 'Create Glossary',
+              description: 'Add a new Glossary'
+            },
+            importLesson:{
+              title: 'Import Lesson',
+              description: 'Import a lesson from library'
+            },
+            importInteraction:{
+              title: 'Import Interaction',
+              description: 'Import an interaction from library'
+            },
+            importGlossary:{
+              title: 'Import Glossary',
+              description: 'Import an interaction from library'
+            },
+          },
         }
       },
       methods:{
-        addSectionClicked(){alert('hello');},
-        addModuleClicked(){alert('hello');},
-        importSectionModuleClicked(){alert('hello');},
-        importModuleClicked(){alert('hello');},
-        addLessonClicked(){alert('hello');},
-        addInteractionClicked(){alert('hello');},
-        addGlossaryClicked(){alert('hello');}
+        addSectionClicked(){
+          this.params.stepper++;
+          this.params.showAddModule = true;
+          },
+        addModuleClicked(){
+          //check the stepper value
+          this.params.stepper++;
+          this.params.showAddContent = true;
+        },
+        importSectionModuleClicked(){
+          // Open Section/Module Search Page
+        },
+        importModuleClicked(){
+          // Open Module Search Page
+        },
+        addLessonClicked(){
+          this.params.stepper++;
+          this.params.showLesson = true
+        },
+        addInteractionClicked(){
+          this.params.stepper++;
+          this.params.showInteractions = true
+        },
+        addGlossaryClicked(){
+          this.params.stepper++;
+          this.params.showGlossary = true
+        },
+        resetParams(){
+          this.params.stepper= 0;
+          this.params.showAddModule=false;
+          this.params.showAddContent=false;
+          this.params.showInteractions= false;
+          this.params.showLesson=false;
+          this.params.showGlossary=false;
+          }
+
+      },
+      mounted() {
+        this.$root.$on('bv::modal::hidden', (bvEvent, modalId) => {
+          this.resetParams();
+
+        })
       }
     }
 </script>
