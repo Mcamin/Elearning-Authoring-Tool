d56b3e4358855675acd142704b74c70efeb6bdbf
Patrick <p.herbke@campus.tu-berlin.de>
2020-02-02 18:57:38 +0100

    New course modal style 80% Notification in course overview 80%

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -1,7 +1,6 @@
 <template>
   <b-container  class=" p-4 my-2">
     <FilterElement/>
-
     <b-row>
       <b-col md="3">
         <CourseCard
@@ -29,6 +28,8 @@
 import CourseCard from "../components/Cards/CourseCard";
 import NewCourseModal from "../components/Modals/NewCourseModal";
 import FilterElement from "../layout/WrapperElements/FilterElement"
+import {Notification} from 'element-ui'
+
 import {mapGetters} from "vuex";
 
   export default {
@@ -49,7 +50,20 @@ import {mapGetters} from "vuex";
           ])
       },
 
+      created() {
+        this.$notify({
+          title: 'Title',
+          message: 'I\'m at the top left corner',
+          position: 'top-left',
+          type: 'info',
+          duration: 0,
+
+        });
+
+    }
+
   };
 </script>
 <style>
+
 </style>
