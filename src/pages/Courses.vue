215aa1d0fb4d40d4e2f513cd8f37804318092284
Patrick <p.herbke@campus.tu-berlin.de>
2020-02-04 10:44:58 +0100

    Notification will be closed when route changes, removed console.log v2

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -90,10 +90,10 @@
                 dangerouslyUseHTMLString: true,
             });
           },
-      beforeRouteLeave (to, from, next) {
-          this.$notify.closeAll();
-          next();
-      }
+        beforeRouteLeave (to, from, next) {
+            this.$notify.closeAll();
+            next();
+        }
     };
 </script>
 <style>
