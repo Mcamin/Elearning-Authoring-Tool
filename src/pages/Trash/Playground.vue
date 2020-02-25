dac1468d2f25258c8896e1545128523b4f39640c
Ahmed Amine <ahmedamine1993@gmail.com>
2020-02-25 13:27:51 +0100

    set routes to home

--- src/pages/Trash/Playground.vue
+++ src/pages/Trash/Playground.vue
@@ -1,6 +1,30 @@
 <template>
-  <b-container fluid class=" mt-5  px-5 ">
 
+  <b-container fluid class=" mt-5 h-100 px-0" style="background: red">
+    <b-row class="h-100">
+      <b-col cols="3"  style="background-color: blue">
+        HEllo
+      </b-col>
+      <b-col style="background: green" class="d-flex align-items-end">
+        <b-card style="width: 50% "  class="mx-2 mb-3">
+          <b-card-body>
+            hello
+          </b-card-body>
+        </b-card>
+        <b-card style="width: 50%" class="mx-2 mb-3">
+          <b-card-body>
+            <el-select class="mx-1"  :placeholder="sortBy[0].label">
+              <el-option
+                v-for="item in sortBy"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </b-card-body>
+        </b-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -11,7 +35,21 @@
     name: "playground",
     components:{  },
     data() {
-      return {}
+      return {
+        sortBy: [
+          {
+            value: 'Recent',
+            label: 'Recently Added'
+          }, {
+            value: 'TitleReversed',
+            label: 'Title: Z-to-A'
+          },
+          {
+            value: 'Title',
+            label: 'Title: A-to-Z'
+          },
+        ],
+      }
     },
 
   };
--- src/router/routes.js
+++ src/router/routes.js
@@ -22,7 +22,7 @@ const routes = [
     {
         path: "/",
         component: Wrapper,
-        redirect: "/playground",
+        redirect: "/home",
         children: [
           {
             path: "home",
